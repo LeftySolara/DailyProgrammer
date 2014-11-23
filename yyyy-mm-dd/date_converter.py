8d7d7d4a5c5565b01f3b893820b36c407327c31b
Jalen Adams <leftysolara@gmail.com>
2014-11-23 14:00:47 -0500

    add regex search

--- yyyy-mm-dd/date_converter.py
+++ yyyy-mm-dd/date_converter.py
@@ -1,13 +1,20 @@
-"""Read in dates, output in format yyyy-mm-dd"""
+""" Read in dates, output in format yyyy-mm-dd
+Years range from 1950-2049
+
+Not the most elegant solution, but I wanted to play 
+with regex and enums
+"""
 
 from enum import Enum
 import sys
+import re
 
 FILENAME = sys.argv[1]
 
-class Months(Enum):
-    JAN, FEB, MAR, APR, MAY, JUN = 1, 2, 3, 4, 5, 6
-    JUL, AUG, SEP, OCT, NOV, DEC = 7, 8, 9, 10, 11, 12
+
+class Month(Enum):
+    Jan, Feb, Mar, Apr, May, Jun = "01", "02", "03", "04", "05", "06"
+    Jul, Aug, Sep, Oct, Nov, Dec = "07", "08", "09", "10", "11", "12"
 
 
 def collect_data(FILENAME):
@@ -15,10 +22,58 @@ def collect_data(FILENAME):
     data = []
     with open(FILENAME) as in_file:
         for line in in_file:
-            data.append(line[:-1])  # remove newline at string end
+            data.append(line[:-1])  # remove newline at string's end
     return data
 
-    
-# data = collect_data("dates.txt")
-# for i in data:
-#     print(i)
+
+def convert_dates(dates, out_file):
+    """Converts given list of dates into format yyyy-mm-dd"""
+    for date in dates:
+        if re.fullmatch("\d{2}/\d{2}/\d{2}", date):  # format mm/dd/yy
+            year = date[-2:]
+            month = date[:2]
+            day = date[3:5]
+
+            if int(year) >= 50:
+                date = "19" + year + '-' + month + '-' + day
+            elif int(date[6:]) <= 49:
+                date = "20" + year + '-' + month + '-' + day
+
+        elif re.fullmatch("\d{2}#\d{2}#\d{2}", date):  # format mm#yy#dd
+            year = date[3:5]
+            month = date[:2]
+            day = date[-2:]
+
+            if int(year) >= 50:
+                date = "19" + year + '-' + month + '-' + day
+            elif int(year) <= 49:
+                date = "20" + year + '-' + month + '-' + day
+
+        elif re.fullmatch("\d{2}\*\d{2}\*\d{4}", date):  # format dd*mm*yyyy
+            year = date[-4:]
+            month = date[3:5]
+            day = date[:2]
+            date = year + '-' + month + '-' + day
+
+        elif re.fullmatch("[A-Z][a-z][a-z]\s\d{2},\s\d{4}", date):  # format word, dd, yyyy
+            year = date[-4:]
+            month = date[:3]
+            day = date[4:6]
+            date = year + '-' + Month[month].value + '-' + day
+
+        elif re.fullmatch("[A-Z][a-z][a-z]\s\d{2},\s\d{2}", date):  # format word, dd, yy
+            year = date[-2:]
+            month = date[:3]
+            day = date[4:6]
+
+            if int(year) >= 50:
+                date = "19" + year + '-' + Month[month].value + '-' + day
+            elif int(year) <= 49:
+                date = "20" + year + '-' + Month[month].value + '-' + day
+
+        out_file.write(date + '\n')
+
+
+date_list = collect_data("dates.txt")
+with open("dates_out.txt", 'w') as out_file:
+    convert_dates(date_list, out_file)
